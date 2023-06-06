9ae8221ac9c7096c08162a1d297d4a784db7c769
domingo2000 <domingo.edwards@uc.cl>
2023-06-05 20:05:16 -0400

    feature: Start script promisified

--- start.js
+++ start.js
@@ -1,25 +1,18 @@
-const { exec } = require("child_process");
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
 
-exec("yarn install", (error, stdout, stderr) => {
-  if (error) {
-    console.log(`error: ${error.message}`);
-    return;
-  }
-  if (stderr) {
-    console.log(`stderr: ${stderr}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-});
+async function deploy() {
+  const { stdout: output1 } = await exec("yarn install");
+  console.log("Installing dependencies...");
+  console.log(output1);
 
-exec("yarn db:deploy", (error, stdout, stderr) => {
-  if (error) {
-    console.log(`error: ${error.message}`);
-    return;
-  }
-  if (stderr) {
-    console.log(`stderr: ${stderr}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-});
+  const { stdout: output2 } = await exec("yarn db:migrate");
+  console.log("Migrating database...");
+  console.log(output2);
+
+  const { stdout: output3 } = await exec("yarn db:seed");
+  console.log("Seeding database...");
+  console.log(output3);
+}
+
+deploy();
